596a366a5e22c4789d93c1c89b505aa694ee35e4
tjsry0466 <tjs0466@naver.com>
2022-09-24 01:25:21 +0900

    [test] addUser

--- test/repository/user.repository.mock.ts
+++ test/repository/user.repository.mock.ts
@@ -2,4 +2,5 @@ import { userData } from '../data/user.data.mock';
 
 export const MockUserRepository = () => ({
   getUserForLogin: jest.fn().mockResolvedValue(userData()[0]),
+  addUser: jest.fn().mockResolvedValue({ ...userData()[0], password: 'hashedPassword' }),
 });
