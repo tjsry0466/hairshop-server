3a4610e6428830e673bfbcca37b2b71282c121cf
Jeong Seonkyo <39974627+tjsry0466@users.noreply.github.com>
2022-09-26 22:10:53 +0900

    id로 유저 조회 구현 (#21)
    * [feat] user query 구현
    
    * [fix] 불필요 role 인증 제거

--- src/user/user.resolver.ts
+++ src/user/user.resolver.ts
@@ -1,6 +1,7 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Query, Resolver } from '@nestjs/graphql';
 
 import { RequestInfo, Roles } from '../common/decorator';
+import { IdArgs } from '../common/dto/id.args';
 import { Role } from '../common/enum';
 import { IRequest } from '../common/interface';
 import { User } from './entity/user.entity';
@@ -9,6 +10,11 @@ import { UserService } from './user.service';
 @Resolver()
 export class UserResolver {
   constructor(private readonly userService: UserService) {}
+
+  @Query(() => User)
+  async user(@Args() args: IdArgs) {
+    return this.userService.getUserById(args.id);
+  }
 
   @Roles(Role.USER)
   @Query(() => User)
