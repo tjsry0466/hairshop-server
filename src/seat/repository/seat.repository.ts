617eaed5ee492df19f85ea8fe5b6ac5eb89819ff
tjsry0466 <tjs0466@naver.com>
2022-10-01 18:03:32 +0900

    좌석 정보 조회 구현

--- src/seat/repository/seat.repository.ts
+++ src/seat/repository/seat.repository.ts
@@ -5,6 +5,10 @@ import { IAddSeatWithInfo } from './interface/add-seat-with-info.interface';
 
 @EntityRepository(Seat)
 export class SeatRepository extends Repository<Seat> {
+  async getOneByShopId(shopId: number) {
+    return this.find({ shopId });
+  }
+
   async addSeat(args: IAddSeatWithInfo) {
     return this.save(this.create(args));
   }
