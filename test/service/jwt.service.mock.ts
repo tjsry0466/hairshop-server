7ce2ada1b3f9400505783195649b701dd1c08785
tjsry0466 <tjs0466@naver.com>
2022-10-03 19:51:34 +0900

    테스트 커버리지 100%

--- test/service/jwt.service.mock.ts
+++ test/service/jwt.service.mock.ts
@@ -1,1 +1,11 @@
-export const MockJwtService = () => ({});
+import { jwtData } from '../data/jwt.data.mock';
+
+export const MockJwtService = () => ({
+  sign: jest.fn().mockImplementation((payload, options) => {
+    if (payload?._refresh === true) {
+      return jwtData().refreshToken;
+    }
+
+    return jwtData().accessToken;
+  }),
+});
