320531cd981ca9b8d64f8d29f11f3de443ae02e9
Seunghun Lee <hun2456@hanmail.net>
2022-10-06 23:19:45 +0900

    fix: resetPassword repository 의 mock 반환값을 affected 로 수정

--- test/repository/user.repository.mock.ts
+++ test/repository/user.repository.mock.ts
@@ -6,7 +6,6 @@ export const MockUserRepository = () => ({
   getUserForLogin: jest.fn().mockResolvedValue(userData()[0]),
   addUser: jest.fn().mockResolvedValue({ ...userData()[0], password: 'hashedPassword' }),
   resetPassword: jest.fn().mockResolvedValue({
-    ...userData()[1],
-    password: '$2a$10$ymqqlTu914LsYh0ElWca7.n07Bo.s1r7RF5RRDufR9SWE.dc3y9gi',
+    affected: 1,
   }),
 });
